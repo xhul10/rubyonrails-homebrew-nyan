af5f819d5c09238084e7d69d99324d2eda869564
Xhuljo Dede <xhulio0031@gmail.com>
2020-02-22 01:07:08 +0900

    Brew formula update for nyan version v0.5.6

--- nyan.rb
+++ nyan.rb
@@ -2,16 +2,16 @@
 class Nyan < Formula
   desc "Colored cat command which supports syntax highlighting."
   homepage "https://github.com/toshimaru/nyan"
-  version "0.5.5"
+  version "0.5.6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/toshimaru/nyan/releases/download/v0.5.5/nyan_Darwin_x86_64.tar.gz"
-    sha256 "0af0641f785b0a5cef06dc6e1bf73ebe04987eb85b8cf50ae1be21ea8c80dc37"
+    url "https://github.com/toshimaru/nyan/releases/download/v0.5.6/nyan_Darwin_x86_64.tar.gz"
+    sha256 "e4ebf3b1dd8682d39e190c1098341f955cec1e02285ab860954e03e5ed3f2662"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/toshimaru/nyan/releases/download/v0.5.5/nyan_Linux_x86_64.tar.gz"
-      sha256 "64ac59aeb00e349dee70aec08684eca13981f702a15f15b53de7eb099eea2258"
+      url "https://github.com/toshimaru/nyan/releases/download/v0.5.6/nyan_Linux_x86_64.tar.gz"
+      sha256 "0c041e5a9236ef690371f0cee851c454f247564ae32a23bf08c030ee7f329cdb"
     end
   end
 
