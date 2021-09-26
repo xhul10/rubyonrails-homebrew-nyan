aeb50eeb4e9621a8705d981db18ac533ee90d0a3
Xhuljo Dede <xhulio0031@gmail.com>
2021-09-27 08:48:44 +0900

    Brew formula update for nyan version v0.9.7

--- nyan.rb
+++ nyan.rb
@@ -5,29 +5,29 @@
 class Nyan < Formula
   desc "Colored cat command which supports syntax highlighting"
   homepage "https://github.com/toshimaru/nyan"
-  version "0.6.7"
+  version "0.9.7"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/toshimaru/nyan/releases/download/v0.6.7/nyan_Darwin_x86_64.tar.gz"
-      sha256 "b2c619abb4abd56a7405e50090d379a6316a05480c739304db81c50d68645424"
+      url "https://github.com/toshimaru/nyan/releases/download/v0.9.7/nyan_Darwin_x86_64.tar.gz"
+      sha256 "e80ea44dab3f1d6805ba3dc33dab896583013b3c3a9a7e093d5d9d274c0ab0b7"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/toshimaru/nyan/releases/download/v0.6.7/nyan_Darwin_arm64.tar.gz"
-      sha256 "2e141216c9d2641202391ce883d9685889f586c3ef8b4e46645f0ab9b590fe03"
+      url "https://github.com/toshimaru/nyan/releases/download/v0.9.7/nyan_Darwin_arm64.tar.gz"
+      sha256 "c2169e0fae6fe1e2cb362708b98342742b74f9ab150e631066391fa7c3e5b2fd"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/toshimaru/nyan/releases/download/v0.6.7/nyan_Linux_x86_64.tar.gz"
-      sha256 "03b1047a112f00edaec3d006361a684ee5abae264953ad9e536dbbb0fd747454"
+      url "https://github.com/toshimaru/nyan/releases/download/v0.9.7/nyan_Linux_x86_64.tar.gz"
+      sha256 "5b970c2c48fbc57a6062c3b21e48edaf9c48fbd995ed1eea0ef0f60695c62632"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/toshimaru/nyan/releases/download/v0.6.7/nyan_Linux_arm64.tar.gz"
-      sha256 "ef1511294b12d74c2335f35230236b08e8507580c14465440e1839581b9b48cf"
+      url "https://github.com/toshimaru/nyan/releases/download/v0.9.7/nyan_Linux_arm64.tar.gz"
+      sha256 "522fd074630794f969a11e518fd71ab8c8a2309bb04a5a63c6d4825cb96fb99b"
     end
   end
 
